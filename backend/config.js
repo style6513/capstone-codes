3ea89d91dfaf02a39d65d80fac9e143ba9c7a838
Dae Young Hwang <daeh@Daes-Air.attlocal.net>
2021-10-18 09:34:34 -0700

    delete folder

--- backend/config.js
+++ backend/config.js
@@ -4,14 +4,14 @@
 
 require('dotenv').config();
 require("colors");
-
+const DEVELOPMENT_DB = process.env.DATABASE_URL || 'code'
 const SECRET = process.env.SECRET_KEY || 'prodKey';
 const PORT = process.env.PORT || 9000;
-const DB_URI = process.env.NODE_ENV === 'test' ? "code_test" : "code";
+const DB_URI = process.env.NODE_ENV === 'test' ? "code_test" : DEVELOPMENT_DB;
 
 // Speed up bcrypt during tests, since algorithm safety isn't being tested.
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
+console.log(DEVELOPMENT_DB)
 console.log('WorkoutBuddy Config'.green);
 console.log('SECRET'.yellow, SECRET);
 console.log('PORT'.yellow, PORT.toString());
